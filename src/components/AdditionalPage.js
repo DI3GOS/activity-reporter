c6c78a467af94021dc7a28a0283854f257426416
DI3GO <ingdi3gosanchez@gmail.com>
2024-05-24 13:36:56 -0500

    s

---
+++ src/components/AdditionalPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdditionalPage = () => {
+  return (
+    <div>
+      <h2>Página Adicional</h2>
+      <p>Esta es una página adicional a la que puedes acceder.</p>
+    </div>
+  );
+};
+
+export default AdditionalPage;
